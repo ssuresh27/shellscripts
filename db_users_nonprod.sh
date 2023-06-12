23907bbff89cd90f2e131d07de3983590d3e746f
ssundararajan02 <Suresh.Sundararajan@gilead.com>
2023-06-11 18:42:33 -0700

    add check user and unlock user function

--- db_users_nonprod.sh
+++ db_users_nonprod.sh
@@ -3,7 +3,7 @@
 
 #Check the user is executed as oracle OS user
 
-USER_NAME='ssundararajan02'
+USER_NAME='oracle'
 PASSWORD_LENGTH=21
 
 if [[ "$(id -un)" != "${USER_NAME}" ]]
@@ -22,12 +22,12 @@ then
 fi
 
 
-# if [[ "$(ps -ef|grep ${ORACLE_SID}|grep -v grep|wc -l)" -ne 1 ]]
-# then
-#     echo "Database instance ${ORACLE_SID} is not running " >&2
-#     exit 1
+if [[ "$(ps -ef|grep ora_pmon_${ORACLE_SID}|grep -v grep|wc -l)" -ne 1 ]]
+then
+    echo "Database instance ${ORACLE_SID} is not running " >&2
+    exit 1
 
-# fi
+fi
 
 #Display usage of script
 
@@ -39,7 +39,8 @@ choice()
     echo -e '\nChoose from the below options'
     echo -e '\t1. Unlock User \t\t\t\t2. Reset user password'
     echo -e '\t3. Check account status \t\t4. Create read-only user'
-    echo -e '\t5. Check user privilege'
+    echo -e '\t5. Check user privilege \t\t6. Press 6 or Q for quit'
+    echo ''
     read -p 'Enter your choice : ' CHOICE
 
 }
@@ -57,22 +58,100 @@ random_password()
 }
 
 #Unlock a database user
-unlock_user()
+execute_sql()
 {
-    sqlplus -s "/as sysdba" << EOF >>/dev/null
-    select "${USERNAME}" from dba_users;
+    local SCRIPT="${SQL}"
+    # echo "${SCRIPT}"
+    DB_RESULT=$(sqlplus -s "/as sysdba" <<EOF
+    set pages 0 lin 1000 feed off ver off head off echo off;
+    set term off
+    $SCRIPT
+    exit
+EOF
+)
+
+#Verify last step status
+if [[ "${?}" -ne 0 ]] 
+then
+    echo 'Error in running SQL' >&2
+    exit 1
+elif [[ $(echo "${DB_RESULT}"|grep ORA-|wc -l) -gt 0 ]]
+then
+    echo "SQL completed with ORA- errror" >&2
+    echo "${DB_RESULT}" >&2
+    exit 1
+fi
+
+# echo 
+# echo "${DB_RESULT}"
+# echo
+# echo
 }
 
+
+check_user_exists()
+{
+    SQL="select username from dba_users where username='${SCHEMA_NAME}';"
+    execute_sql
+    if [[ -z ${DB_RESULT} ]]
+    then 
+        echo "${SCHEMA_NAME} not exists in DB"
+        DB_RESULT=1
+    else
+        echo "${SCHEMA_NAME} exists in DB"
+        DB_RESULT=0
+    fi
+}
+
+
+check_user_account_status()
+{
+    check_user_exists
+    if [[ "${DB_RESULT}" -eq 1 ]]
+    then
+        # exit 1
+        return #replacing with return to skip breaking the script
+    fi
+    SQL="select username,account_status,lock_date,last_login from dba_users where username='${SCHEMA_NAME}';"
+    execute_sql
+    ACCOUNT_STATUS=$(echo "${DB_RESULT}"|awk '{print $2}')
+    echo "${SCHEMA_NAME} account is ${ACCOUNT_STATUS} state"
+
+}
+
+
+unlock_user()
+{
+    # check_user_exists
+    check_user_account_status
+    if [[ "${ACCOUNT_STATUS}" = 'OPEN' ]]
+    then
+        echo "${SCHEMA_NAME} account is already open"
+        echo "${DB_RESULT}"
+        # exit 0
+        return #replacing with return to skip breaking the script
+    else
+        SQL="alter user ${SCHEMA_NAME} account unlock;"
+        execute_sql
+        # echo "${DB_RESULT}"
+        check_user_account_status
+
+    fi
+
+}
+
+QUIT='n'
+while [[ "${QUIT}"  != 'y' ]]
+do
 choice
 # echo "${CHOICE}"
 # echo "${SSH_CLIENT}"
 case "${CHOICE}" in
     
     1)
-        echo 'Ulock user:'
+        echo -e '\nUlock user:'
         read_schema_name
-        random_password
-        echo -e "${SCHEMA_NAME} \t ${PASSWORD}"
+        unlock_user
         ;;
     2)
         echo 'Reset user password'
@@ -80,9 +159,10 @@ case "${CHOICE}" in
         echo "${SCHEMA_NAME}"
         ;;
     3) 
-        echo 'Check user account status'
+        echo -e '\nCheck user account status'
         read_schema_name
-        echo "${SCHEMA_NAME}"
+        check_user_account_status
+        # echo "${SCHEMA_NAME}"
         ;;
     4) 
         echo 'Create read-only user'
@@ -93,10 +173,13 @@ case "${CHOICE}" in
         echo 'Check user privilege'
         echo "${SCHEMA_NAME}"
         ;;
+    6|q)
+        QUIT='y'
+        ;;
     ?)
         echo 'Invalid chooice' >&2
         exit 1
         ;;
 
-
 esac
+done
